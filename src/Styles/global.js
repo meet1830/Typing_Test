8cf4338858c158b2778d8b20f3e4579213f5879b
meet1830 <mehtameet1234@gmail.com>
2022-11-09 12:39:32 +0530

    Lecture 01 - 8/11/2022 - Create React App, installed dependencies, initialized global styles

---
+++ src/Styles/global.js
@@ -0,0 +1,5 @@
+import { createGlobalStyle } from "styled-components";
+// placing it at the top of the App.js file, the styles will be applied to all the components after it
+
+export const GlobalStyles = createGlobalStyle`
+`;
